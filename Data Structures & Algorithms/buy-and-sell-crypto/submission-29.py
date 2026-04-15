02ad409dc0a50c2a5884ae1cf848fbd1b96b5885
abdurahmanjamae <44628394+abdurahmanjamae@users.noreply.github.com>
2026-04-15 11:09:59 -0700

    Add: buy-and-sell-crypto - submission-29

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-29.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # left = buy day, right = sell day
+        left, right = 0, 1 
+        res = 0 
+
+        while right < len(prices):
+            # If profitable, calculate profit and update our max record
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                res = max(res, profit)
+            else:
+                # Found a lower buying price, so shift the window to start from this new minimum.
+                left = right
+            
+            # Always move the right pointer to explore the next selling day
+            right += 1
+            
+        return res
+        
+        
