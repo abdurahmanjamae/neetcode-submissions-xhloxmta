75ac6b10e8610f1da714d591c70851e31fd24a79
abdurahmanjamae <44628394+abdurahmanjamae@users.noreply.github.com>
2026-04-30 17:10:12 -0700

    Add: lowest-common-ancestor-in-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        # Base case: if we hit a null node
+        if not root or not p or not q:
+            return None
+            
+        # Both values are smaller than root; LCA must be in the left subtree
+        if (max(p.val, q.val) < root.val):
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        # Both values are larger than root; LCA must be in the right subtree
+        elif (min(p.val, q.val) > root.val):
+            return self.lowestCommonAncestor(root.right, p, q)
+        
+        # We found the "split" point (or root is p or q); this is the LCA
+        else:
+            return root
+
+# Time Complexity: O(h), where h is the height of the tree. 
+# We visit one node per level. In a balanced tree, this is O(log n).
+# Space Complexity: O(h), due to the recursion stack depth. 
+# (An iterative version would bring this down to O(1)).
